1fc1e9607cc4a30d4140d3560f875dd8e779d50a
wmiq84 <michaelwang3489@gmail.com>
2024-09-08 14:00:53 -0700

    Complete non-GUI board functionality

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+const uiBoard = document.querySelector(".board");
+
 const gameboard = (function () {
     const row = 3;
     const col = 3;
     const board = [];
 
     function createBoard(row, col) {
-        for (let i = 0; i < row; i++) {
+        for (var i = 0; i < row; i++) {
             board[i] = [];
-            for (let j = 0; j < col; j++)
+            for (var j = 0; j < col; j++) {
                 board[i].push(0); 
+
+                // add tiles to GUI
+                let newTile = document.createElement('label');
+                newTile.textContent = `${i}${j}`;
+                newTile.classList.add('tile');
+                newTile.dataset.index = `${i}${j}`;
+                newTile.addEventListener('click', event => {
+                    console.log(newTile.dataset.index);
+                    game.placePiece(newTile.dataset.index[0], newTile.dataset.index[1], board, playerOne);
+                });
+                uiBoard.appendChild(newTile);
             }
         }
-
+    }
+    console.log(uiBoard);
     createBoard(row, col)
 
     return {
@@ -21,6 +35,88 @@ const gameboard = (function () {
     }
 })();
 
+function createPlayer(name, isTurn) {
+    return {
+        name,
+        isTurn
+    }
+}
+
+const game = (function () {
+    function placePiece(row, col, board, player) {
+        // const row = prompt("Enter player one name: ");
+        // const col = prompt("Enter player two name: ");
+        // decrements index by 1 to make selections match gameboard start at index 1
+        // row--;
+        // col--;
+    
+        board[row][col] = (player === true ? 1 : -1);
+        (player == true ? false: true);
+        console.log(board);
+        console.log(checkWin(board));
+        console.log("p1: " + playerOne.isTurn)
+        console.log("p2: " + playerTwo.isTurn)
+
+        return {
+            board
+        }
+    }
+    function checkWin(board) {
+    //check rows
+    for (let i = 0; i < board.length; i++) {
+        if (board[i][0] != 0 && 
+            board[i][0] == board[i][1] && 
+            board[i][1] == board[i][2]) {
+            return true;
+        }
+    }
+    //check columns
+    for (let j = 0; j < board.length; j++) {
+        if (board[0][j] != 0 && 
+            board[0][j] == board[1][j] && 
+            board[1][j] == board[2][j]) {
+            return true;
+        }
+    }
+    //check diagonals
+    if (board[0][0] != 0 && 
+        board[0][0] == board[1][1] && 
+        board[1][1] == board[2][2]) {
+        return true;
+    }
+    if (board[0][2] != 0 && 
+        board[0][2] == board[1][1] && 
+        board[1][1] == board[2][0]) {
+        return true;
+    }
+    }
+    return {
+        placePiece,
+        checkWin
+    }
+})();
+
+// function screenController() {
+
+// }
+
+
+
+
+
+const board = gameboard.board;
+
+const playerOne = createPlayer("One", true)
+const playerTwo = createPlayer("Two", false);
+
+// game.placePiece(1, 1, board, playerOne);
+
+// console.log(game.checkWin(board));
+// game.placePiece(2, 2, board, playerOne);
+
+// console.log(game.checkWin(board));
+// game.placePiece(2, 3, board, playerOne);
+console.log(game.checkWin(board));
 
 // const players = (function () {
 //     function createPlayer() {
@@ -48,99 +144,3 @@ const gameboard = (function () {
 //         createPlayerInstance
 //     }
 // })();
-
-function createPlayer(name) {
-    return {
-        name,
-        isTurn: false
-    }
-}
-
-const game = (function () {
-    function placePiece(row, col, board, player) {
-        // const row = prompt("Enter player one name: ");
-        // const col = prompt("Enter player two name: ");
-        // decrements index by 1 to make selections match gameboard start at index 1
-        row--;
-        col--;
-    
-        board[row][col] = (player === true ? 1 : -1);
-        console.log(board);
-        // checkWin(board);
-    
-        return {
-            board
-        }
-    }
-    
-    function checkWin(board) {
-    //check rows
-    for (let i = 0; i < board.length; i++) {
-        if (board[i][0] != 0 && 
-            board[i][0] == board[i][1] && 
-            board[i][1] == board[i][2]) {
-            return true;
-        }
-    }
-
-    //check columns
-    for (let j = 0; j < board.length; j++) {
-        if (board[0][j] != 0 && 
-            board[0][j] == board[1][j] && 
-            board[1][j] == board[2][j]) {
-            return true;
-        }
-    }
-
-    //check diagonals
-    if (board[0][0] != 0 && 
-        board[0][0] == board[1][1] && 
-        board[1][1] == board[2][2]) {
-        return true;
-    }
-    if (board[0][2] != 0 && 
-        board[0][2] == board[1][1] && 
-        board[1][1] == board[2][0]) {
-        return true;
-    }
-    }
-    return {
-        placePiece,
-        checkWin
-    }
-})();
-
-const board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
-
-const playerOne = createPlayer("One")
-const playerTwo = createPlayer("Two");
-
-// const playersInstance = players.createPlayerInstance;
-game.placePiece(1, 1, board, playerOne);
-// console.log(playersInstance);
-// console.log(playersInstance.playerOne);
-console.log(game.checkWin(board));
-// game.placePiece(1, 2, board, playersInstance.playerOne);
-game.placePiece(1, 2, board, playerOne);
-
-console.log(game.checkWin(board));
-// game.placePiece(1, 3, board, playersInstance.playerOne);
-game.placePiece(1, 3, board, playerOne);
-console.log(game.checkWin(board));
-
-
-// game.placePiece(1, 1, board, true);
-// console.log(gameboard.board);
-
-// placePiece(1, 2, gameboard.board, playerOne);
-// createPlayer();
-
-/* Set up your project with HTML, CSS and Javascript files and get the Git repo all set up.
-You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
-Your main goal here is to have as little global code as possible. Try tucking as much as you can inside factories. If you only need a single instance of something (e.g. the gameboard, the displayController etc.) then wrap the factory inside an IIFE (module pattern) so it cannot be reused to create additional instances.
-In this project, think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects. Take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-If you’re having trouble, Building a house from the inside out is a great article that lays out a highly applicable example both of how you might approach tackling this project as well as how you might organize and structure your code. */
